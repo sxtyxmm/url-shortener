3a5ebe192a93c874d951fa4e8462c0869def2bfe
Satyam Shishodiya <105547704+sxtyxmm@users.noreply.github.com>
2025-06-03 14:14:56 +0000

    fix: add missing import for Optional in UrlController

--- src/main/java/com/urlefy/controller/UrlController.java
+++ src/main/java/com/urlefy/controller/UrlController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
